c70a3e6e8e9061f01c5cbc50e4681f54d258386e
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-05-09 03:58:03 -0400

    add search

--- resolvers/GroupChatResolver.ts
+++ resolvers/GroupChatResolver.ts
@@ -1,57 +1,102 @@
-import { Resolver, Query, Arg, Mutation  } from "type-graphql";
-import { GroupChat as GroupChatModel, User as UserModel } from '../database';
-import { GroupChat } from '../models';
-import { createGroupChatInput } from '../inputs';
+import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { GroupChat as GroupChatModel, User as UserModel } from "../database";
+import { GroupChat } from "../models";
+import { createGroupChatInput } from "../inputs";
 import { GroupChatPaginiated } from "../models/Groupchat";
 
-
+function escapeRegex(text: any) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 @Resolver(GroupChat)
 export class GroupChatResolver {
   pageSize = 8;
 
   @Query(() => GroupChatPaginiated)
-  async getGroupChats(@Arg("page", {nullable: true}) page: number = 0){
-    const groupChats = await GroupChatModel.find().skip(page * this.pageSize).limit(this.pageSize);
+  async getGroupChats(@Arg("page", { nullable: true }) page: number = 0) {
+    const groupChats = await GroupChatModel.find()
+      .skip(page * this.pageSize)
+      .limit(this.pageSize);
     const totalCount = await GroupChatModel.find().countDocuments();
-    return {groupChats, totalPages: Math.ceil(totalCount / this.pageSize) - 1 , pageNumber: page};
+    if (totalCount === 0){
+      return {
+        groupChats: [],
+        totalPages: 0,
+        pageNumber: 0
+      }
+    }
+    return {
+      groupChats,
+      totalPages: Math.ceil(totalCount / this.pageSize) - 1,
+      pageNumber: page,
+    };
   }
-  @Query(()=> GroupChat, {nullable: true})
-  async getGroupChat(@Arg("id") id: string){
+  @Query(() => GroupChat, { nullable: true })
+  async getGroupChat(@Arg("id") id: string) {
     const GroupChat = await GroupChatModel.findOne({ _id: id });
     return GroupChat;
   }
 
-  @Query(()=> [GroupChat], {nullable: true})
-  async getGroupChatByStatus(@Arg("status") status:string){
+  @Query(() => [GroupChat], { nullable: true })
+  async getGroupChatByStatus(@Arg("status") status: string) {
     const GroupChat = await GroupChatModel.find({ status });
     return GroupChat;
   }
 
+  @Query(() => GroupChatPaginiated, { nullable: true })
+  async searchGroupChats(
+    @Arg("text") text: string,
+    @Arg("isCommunity", { nullable: true }) type?: Boolean,
+    @Arg("page", { nullable: true }) page: number = 0
+  ) {
+    const regex = new RegExp(escapeRegex(text), "gi");
+    let queryObj = {};
+    queryObj = { description: regex };
+    if (type != undefined) {
+      queryObj = { ...queryObj, isCommunity: type };
+    }
+    const groupChats = await GroupChatModel.find(queryObj)
+      .skip(page * this.pageSize)
+      .limit(this.pageSize);
+    const totalCount = await GroupChatModel.find(queryObj).countDocuments();
+    if (totalCount === 0){
+      return {
+        groupChats: [],
+        totalPages: 0,
+        pageNumber: 0
+      }
+    }
+    return {
+      groupChats,
+      totalPages: Math.ceil(totalCount / this.pageSize) - 1,
+      pageNumber: page,
+    };
+  }
 
-
-  @Mutation(()=> GroupChat, {nullable: true})
-  async addGroupChat(@Arg("email") email: string, @Arg("info") groupchatInfo: createGroupChatInput){
+  @Mutation(() => GroupChat, { nullable: true })
+  async addGroupChat(
+    @Arg("email") email: string,
+    @Arg("info") groupchatInfo: createGroupChatInput
+  ) {
     const user = UserModel.findOne({ email });
-    if (!user){
+    if (!user) {
       return null;
     }
-    const newGroupChat = await GroupChatModel.create({...groupchatInfo});
+    const newGroupChat = await GroupChatModel.create({ ...groupchatInfo });
     await UserModel.updateOne(
-      { email}, 
-      { $push: { groupChatsCreated: newGroupChat._id } },
+      { email },
+      { $push: { groupChatsCreated: newGroupChat._id } }
     );
     return newGroupChat;
   }
 
-  @Mutation(()=> GroupChat, {nullable: true})
-  async updateGroupChat(@Arg("id") id: string, @Arg("status") status: string){
+  @Mutation(() => GroupChat, { nullable: true })
+  async updateGroupChat(@Arg("id") id: string, @Arg("status") status: string) {
     const groupChat = await GroupChatModel.findOne({ _id: id });
-    if (!groupChat){
+    if (!groupChat) {
       return null;
     }
     groupChat.status = status;
     const result = await groupChat.save();
     return result;
   }
-
 }
